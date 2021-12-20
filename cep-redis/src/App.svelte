20b737b4d6fa9e11bd526478b6fd3a65c5faa4c9
Anastacia Menezes <anastacia.azevedo@estudante.ifms.edu.br>
2021-12-20 00:49:58 -0300

    commit inicial

---
+++ cep-redis/src/App.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { Router, Route } from "svelte-navigator";
+  import Header from "./components/Header.svelte";
+  import Home from "./pages/Home.svelte";
+</script>
+
+<Header />
+
+<Router>
+    <Route to="/" component={Home} />
+</Router>
---
+++ cep-redis/src/components/Header.svelte
@@ -0,0 +1,18 @@
+<div class="header">
+    <h1>CEP Redis</h1>
+</div>
+
+<style lang="scss">
+    .header {
+        padding: 10px 20px;
+        background: linear-gradient(45deg, #663399, #9f5afd);
+
+        h1{
+            font-family: "title";
+            color: white;
+            font-size: 300%;
+            text-shadow: 3px 5px #333;
+
+        }
+    }
+</style>
